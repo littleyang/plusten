9e0288664e7d7648a9a4cbc86a364e5b8ac0f95b
littleyang <littleyangqh@gmail.com>
2012-10-10 15:13:03 +0800

    plusten game

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pulsten::Application.config.secret_token = 'f7f8461283d33fdfdc9923e7ca9a214b731a62f8b6e2ea1015caee9486c42c1d4dd507d058693eb9e2672e3879a909d231bbd35691d138aef7a82eaf42bfd904'
