35051f843049cbf253a30db5d310b431f6c9a0fe
littleyang <littleyangqh@gmail.com>
2012-10-18 21:36:10 +0800

    how to conut current_user's current_game_count?

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -12,16 +12,20 @@ class RoomController < ApplicationController
   end
 
   def single_user_room
-    render :template=>'room/single_player'
+    @types = Type.order("id DESC")
+    respond_to do |format|
+      format.html { render :template=>'room/single_player'}
+      format.json { render json: @types }
+    end
     session[:game_type] = "single_player"
   end
 
   def current_single_game
     #@single_game = current_user.single_game.order("id DESC").first
     #session[:game_type_id]=params[:type]
+    #session[:game_count]=0
   end
   def post_single
-    #@game = params[:data]
     if request.post?
       @single_game = SingleGame.new
       @single_game.user = current_user
@@ -29,15 +33,15 @@ class RoomController < ApplicationController
       @single_game.time = params[:thought_time]
       @single_game.game_type = params[:type]
       @single_game.score = params[:score]
+      @single_game.current_count = (current_user.single_game).count
+      @single_game.game_total = (Type.find_by_id(params[:type])).total_num
+      puts @single_game.count
+      puts @single_game.game_total
       if @single_game.save!
           respond_to do |format|
             format.json{ render json: @single_game }
           end
       end
     end
-    #puts @single_game
-    #puts params[:select_num]
-    #puts params[:thought_time]
-    #puts @game
   end
 end
--- app/models/single_game.rb
+++ app/models/single_game.rb
@@ -1,6 +1,8 @@
 class SingleGame < ActiveRecord::Base
   attr_accessible :score, :select_num, :time, :game_type, :user_id
   belongs_to :user
+  attr_accessor :game_total
+  attr_accessor :current_count
   def caculate_score(num,type)
       if (type == 1)
         score = 50 - num
