9a6e981f25ff0c2752752791b1e9fb41b2e3bf99
littleyang <littleyangqh@gmail.com>
2012-10-18 16:46:39 +0800

    add single_game type model ,in case to control total game count

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018041215) do
+ActiveRecord::Schema.define(:version => 20121018084430) do
 
   create_table "games", :force => true do |t|
     t.integer  "score"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(:version => 20121018041215) do
     t.string   "game_type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "types", :force => true do |t|
+    t.string   "name"
+    t.integer  "total_num",  :default => 30
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "user_games", :force => true do |t|
